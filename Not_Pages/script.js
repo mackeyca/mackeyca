1a1fe809be55fa0eeed9d74d23c16b7e44e3e448
mackeyca <mackeyca@mail.uc.edu>
2024-12-11 23:27:56 -0500

    updated footer and fixed script

--- Not_Pages/script.js
+++ Not_Pages/script.js
@@ -10,72 +10,72 @@ const Job1Holder = document.getElementById('Job1');
 buttonResume.addEventListener('click', function () {
     if (ResumeHolder.classList.contains('ResumeOff')) {
         ResumeHolder.classList.remove('ResumeOff');
-        ResumeHolder.classList.add('ResumeOn');
+        ResumeHolder.classList.add('ResumeBox');
     }
-    if (Job3Holder.classList.contains('ResumeOn')) {
-        Job3Holder.classList.remove('ResumeOn');
+    if (Job3Holder.classList.contains('ResumeBox')) {
+        Job3Holder.classList.remove('ResumeBox');
         Job3Holder.classList.add('ResumeOff');
     }
-    if (Job2Holder.classList.contains('ResumeOn')) {
-        Job2Holder.classList.remove('ResumeOn');
+    if (Job2Holder.classList.contains('ResumeBox')) {
+        Job2Holder.classList.remove('ResumeBox');
         Job2Holder.classList.add('ResumeOff');
     }
-    if (Job1Holder.classList.contains('ResumeOn')) {
-        Job1Holder.classList.remove('ResumeOn');
+    if (Job1Holder.classList.contains('ResumeBox')) {
+        Job1Holder.classList.remove('ResumeBox');
         Job1Holder.classList.add('ResumeOff');
     }
 });
 buttonJob3.addEventListener('click', function () {
     if (Job3Holder.classList.contains('ResumeOff')) {
         Job3Holder.classList.remove('ResumeOff');
-        Job3Holder.classList.add('ResumeOn');
+        Job3Holder.classList.add('ResumeBox');
     }
-    if (ResumeHolder.classList.contains('ResumeOn')) {
-        ResumeHolder.classList.remove('ResumeOn');
+    if (ResumeHolder.classList.contains('ResumeBox')) {
+        ResumeHolder.classList.remove('ResumeBox');
         ResumeHolder.classList.add('ResumeOff');
     }
-    if (Job2Holder.classList.contains('ResumeOn')) {
-        Job2Holder.classList.remove('ResumeOn');
+    if (Job2Holder.classList.contains('ResumeBox')) {
+        Job2Holder.classList.remove('ResumeBox');
         Job2Holder.classList.add('ResumeOff');
     }
-    if (Job1Holder.classList.contains('ResumeOn')) {
-        Job1Holder.classList.remove('ResumeOn');
+    if (Job1Holder.classList.contains('ResumeBox')) {
+        Job1Holder.classList.remove('ResumeBox');
         Job1Holder.classList.add('ResumeOff');
     }
 });
 buttonJob2.addEventListener('click', function () {
     if (Job2Holder.classList.contains('ResumeOff')) {
         Job2Holder.classList.remove('ResumeOff');
-        Job2Holder.classList.add('ResumeOn');
+        Job2Holder.classList.add('ResumeBox');
     }
-    if (Job3Holder.classList.contains('ResumeOn')) {
-        Job3Holder.classList.remove('ResumeOn');
+    if (Job3Holder.classList.contains('ResumeBox')) {
+        Job3Holder.classList.remove('ResumeBox');
         Job3Holder.classList.add('ResumeOff');
     }
-    if (ResumeHolder.classList.contains('ResumeOn')) {
-        ResumeHolder.classList.remove('ResumeOn');
+    if (ResumeHolder.classList.contains('ResumeBox')) {
+        ResumeHolder.classList.remove('ResumeBox');
         ResumeHolder.classList.add('ResumeOff');
     }
-    if (Job1Holder.classList.contains('ResumeOn')) {
-        Job1Holder.classList.remove('ResumeOn');
+    if (Job1Holder.classList.contains('ResumeBox')) {
+        Job1Holder.classList.remove('ResumeBox');
         Job1Holder.classList.add('ResumeOff');
     }
 });
 buttonJob1.addEventListener('click', function () {
     if (Job1Holder.classList.contains('ResumeOff')) {
         Job1Holder.classList.remove('ResumeOff');
-        Job1Holder.classList.add('ResumeOn');
+        Job1Holder.classList.add('ResumeBox');
     }
-    if (Job3Holder.classList.contains('ResumeOn')) {
-        Job3Holder.classList.remove('ResumeOn');
+    if (Job3Holder.classList.contains('ResumeBox')) {
+        Job3Holder.classList.remove('ResumeBox');
         Job3Holder.classList.add('ResumeOff');
     }
-    if (Job2Holder.classList.contains('ResumeOn')) {
-        Job2Holder.classList.remove('ResumeOn');
+    if (Job2Holder.classList.contains('ResumeBox')) {
+        Job2Holder.classList.remove('ResumeBox');
         Job2Holder.classList.add('ResumeOff');
     }
-    if (ResumeHolder.classList.contains('ResumeOn')) {
-        ResumeHolder.classList.remove('ResumeOn');
+    if (ResumeHolder.classList.contains('ResumeBox')) {
+        ResumeHolder.classList.remove('ResumeBox');
         ResumeHolder.classList.add('ResumeOff');
     }
 });
